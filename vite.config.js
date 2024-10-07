c3e6508a983b1be03f401ea5f42d910d5fb42c6c
linkin1650 <linkin1650@gmail.com>
2024-10-08 01:03:56 +0800

    revise vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // <https://vitejs.dev/config/>
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/alpha-shop' : '/',
   plugins: [react()],
-  base: '/alpha-shop',
   server: {
     port: 3000
   }
